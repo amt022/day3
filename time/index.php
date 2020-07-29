de11975a2ecb853cf5043ef9f736b60857ef6f97
amt022 <63891723+amt022@users.noreply.github.com>
2020-07-29 10:39:09 +0530

    Delete index.php

--- time/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$time = time();
-$actualTime = date('d M Y @ H:i:s',$time);
-$modifiedTime = date('d M Y @ H:i:s', strtotime('+1 week'));
-
-echo 'current date/time is '. $actualTime.'<br>';
-echo 'modified date/time is '. $modifiedTime;
-?>
